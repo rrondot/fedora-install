9a232e0e46fe52896d1e748c6ae820ab8d971428
rrondot <remirondot13@gmail.com>
2026-02-09 19:21:51 +0100

    modif starship

--- install.sh
+++ install.sh
@@ -64,15 +64,10 @@ sudo dnf install -y nodejs
 
 sudo dnf install -y python3.11 python3.11-pip
 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+curl -sS https://starship.rs/install.sh | sh -s -- -y
 
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="powerlevel10k\/powerlevel10k"/' ~/.zshrc
-
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-
-sed -i 's/plugins=(git)/plugins=(git docker kubectl terraform ansible zsh-autosuggestions zsh-syntax-highlighting)/' ~/.zshrc
+echo 'eval "$(starship init bash)"' >> ~/.bashrc
+echo 'eval "$(starship init zsh)"' >> ~/.zshrc
 
 chsh -s $(which zsh)
 
